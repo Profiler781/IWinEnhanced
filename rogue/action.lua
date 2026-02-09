a6bbaafd3999a81f83bdae928799fe3c6e025772
Agamemnoth <Profiler781@users.noreply.github.com>
2026-02-09 10:50:25 +0100

    oops

--- rogue/action.lua
+++ rogue/action.lua
@@ -273,7 +273,7 @@ function IWin:SetReservedEnergyRupture()
 			IWin:GetTalentRank(1, 10) == 3
 			or (
 					IWin:GetTimeToDie() > 13
-					and not IWin:IsBuffActive("Rupture", "target", "player")
+					and not IWin:IsBuffActive("target", "Rupture", "player")
 				)
 		)
 		and GetComboPoints() == 5
